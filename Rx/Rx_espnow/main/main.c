89ff6a6c3bdc15631675e9340fd2a1446fc01259
ThaiToanSW <vothaitoan2004@gmail.com>
2026-03-19 17:31:49 +0700

    hello world

--- Rx/Rx_espnow/main/main.c
+++ Rx/Rx_espnow/main/main.c
@@ -1,6 +1,62 @@
 #include <stdio.h>
+#include "esp_wifi.h"
+#include "esp_mac.h"
+#include "esp_log.h"
+#include "string.h"
+#include "nvs_flash.h"
+#include "esp_now.h"
+
+// declare functions
+void send_cb(const esp_now_send_info_t *info, esp_now_send_status_t status);
+void recv_cb(const esp_now_recv_info_t *info, const uint8_t *data, int len);
+
+void wifi_init(void)
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+        {
+            nvs_flash_erase();
+            ret = nvs_flash_init();
+        }
+
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_start();
+
+    esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE); // cùng channel
+}
+
+void espnow_init(void)
+{
+    esp_now_init();
+    esp_now_register_send_cb(send_cb);
+    esp_now_register_recv_cb(recv_cb);
+}
+
+// MAC TX
+uint8_t peer_mac[6] = {0xF0, 0x24, 0xF9, 0xEB, 0x6F, 0x6C};
+
+// receive callback function
+void recv_cb(const esp_now_recv_info_t *info,
+             const uint8_t *data, int len)
+{
+    printf("Received: %.*s\n", len, data);
+}
+
+// send callback function
+void send_cb(const esp_now_send_info_t *info, esp_now_send_status_t status)
+{
+    
+}
 
 void app_main(void)
 {
-
+    wifi_init();
+    espnow_init();
 }
