3f24a5e67bb94257d6bc46de90b1b0f9b5ec4f77
ThaiToanSW <vothaitoan2004@gmail.com>
2026-03-19 09:46:55 +0700

    code get mac addr

--- Tx/Tx_espnow/main/main.c
+++ Tx/Tx_espnow/main/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
-
+#include "esp_wifi.h"
+#include "esp_mac.h"
+#include "esp_log.h"
+#include "string.h"
+#include "nvs_flash.h"
+#include "esp_now.h"
+// transmitter
 void app_main(void)
 {
-    
+
+    uint8_t mac[6];
+    esp_wifi_get_mac(WIFI_IF_STA, mac);
+
+    printf("%02X:%02X:%02X:%02X:%02X:%02X\n",
+       mac[0], mac[1], mac[2],
+       mac[3], mac[4], mac[5]);
 }
