1586bcd31d369e1c64eee55592cf96b5beeaf434
ThaiToanSW <vothaitoan2004@gmail.com>
2026-03-19 09:58:41 +0700

    change function read mac addr

--- Tx/Tx_espnow/main/main.c
+++ Tx/Tx_espnow/main/main.c
@@ -5,14 +5,14 @@
 #include "string.h"
 #include "nvs_flash.h"
 #include "esp_now.h"
-// transmitter
+#include "esp_system.h"
+// receiver
 void app_main(void)
 {
-
     uint8_t mac[6];
-    esp_wifi_get_mac(WIFI_IF_STA, mac);
+    esp_read_mac(mac, ESP_MAC_WIFI_STA);
 
     printf("%02X:%02X:%02X:%02X:%02X:%02X\n",
-       mac[0], mac[1], mac[2],
-       mac[3], mac[4], mac[5]);
+           mac[0], mac[1], mac[2],
+           mac[3], mac[4], mac[5]);
 }
